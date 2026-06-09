245a7506db8129ed801f3fac329dd4fd43bd40fe
MightyManrock <MightyManrock@gmail.com>
2026-06-09 16:27:17 -0400

    Initial species enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+pub enum SpeciesKind {
+    Named,
+    Generic,
+}
+
+pub enum LifeBasis {
+    Carbon,
+    Silicon,
+    Arsenic,
+    Borane,
+    Sulfur,
+    Phosphorus,
+}
+
+pub enum Solvent {
+    Water,
+    Ammonia,
+    Methane,
+    HydrogenFluoride,
+    HydrogenSulfide,
+    SiliconDioxide,
+    SulfuricAcid,
+    Formamide,
+}
+
+pub enum AtmosphereTag {
+    Oyxgen,
+    Nitrogen,
+    CarbonMonoxide,
+    CarbonDioxide,
+    Methane,
+    Fumarate,
+    Sulfate,
+}
+
+pub enum AtmosphereRelationship {
+    Required,
+    Beneficial,
+    Tolerated,
+    Toxic
+}
+
+pub enum RespirationMedium {
+    Gas,
+    Liquid,
+    Solid,
+    Vacuum,
+}
+
 fn main() {
     println!("Hello, world!");
 }
