4e57436c6246eb84a20d830454d85b5936b3a5dd
MightyManrock <MightyManrock@gmail.com>
2026-06-09 23:17:02 -0400

    Update mortal.rs

--- src/mortal.rs
+++ src/mortal.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
@@ -17,13 +18,10 @@ pub enum DivineStatus {
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
-pub struct DivineSpark {
-    pub role: Option<DivineRole>,
-    pub status: Option<DivineStatus>,
-    pub appointed_tick: Option<u32>,    // Divine powers unlocked?
-    pub alignment: f32,                 // Imāginēs known? I think
-    pub loyalty: Option<f32>,           // those might be better as
-    pub last_audit_tick: Option<u32>,   // part of the MortalAgent.
+pub struct DivineSpark {                // More "divine relational" fields to be on
+    pub role: Option<DivineRole>,       // MortalState, as well as things like divine
+    pub status: Option<DivineStatus>,   // powers unlocked, Imāginēs know (if applicable),
+    pub appointed_tick: Option<u32>,    // etc.
 }
 
 #[derive(Debug)]
@@ -54,29 +52,49 @@ pub struct BandAffiliation {
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 pub struct MortalIdentity {
-    pub beliefs: Vec<String>,       // Gender will likely end up drawing on aspects
-    pub culture: Vec<String>,       // of the parent civilization. Might als encompass
-    pub personality: Vec<String>,   // sexuality if that is ever relevant to the
-    pub gender: String,             // simulation.
+//  pub beliefs: HashMap<Belief, f32>,  // Belief and Culture enums to be shared
+//  pub culture: HashMap<Culture, f32>, // by all entities (Civs, Pops, etc.)
+//  pub personality: HashMap<Personality, f32>,
+//  pub gender: MortalGender,
+}
+
+// Gender will likely end up drawing on aspects of the parent
+// civilization. Might also encompass sexuality if that is ever
+// relevant to the simulation.
+
+#[derive(Debug)]
+#[derive(Serialize, Deserialize)]
+pub enum Skill {
+    // Material & Occupational
+    Trade,
+    Craft,
+    Labor,
+    Navigation,
+    Engineering,
+    Medicine,
+    Combat,
+    // Social
+        // Pop-facing
+        Rhetoric,
+        Ritual,
+        Performance,
+        // Faction-facing
+        Leadership,
+        Diplomacy,
+    // Covert
+    Stealth,
+    // Knowledge
+    Scholarship,
 }
 
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
-pub struct MortalCapability {
-    pub abilities: Vec<String>,     // Iffy on `abilities`; if implemented, maybe
-    pub occupation: Vec<String>,    // this should be a struct with pre-set "ability
-    pub skills: Vec<String>,        // scores," like an RPG character's?
-    pub status: Vec<String>,        // I.e., social status, some special societal
-}                                   // role beyond factional.
-
-#[derive(Debug)]
-#[derive(Serialize, Deserialize)]
 pub struct MortalAge {
-    pub birthday_billions: u32,     // `bio_age` as a property derived from
-    pub birthday_millions: u32,     // in-game date - birthday`? `chrono_age`
-    pub birthday_thousands: u32,    // derived either as copy of `bio_age` or
-    pub birthday_years: u32,        // calculated with div_spark.appointed_tick
-    pub birthday_month: u32,        // considered?
+    pub birthday_billions: Option<u32>,     // `bio_age` as a property derived from
+    pub birthday_millions: Option<u32>,     // in-game date - birthday`? `chrono_age`
+    pub birthday_thousands: Option<u32>,    // derived either as copy of `bio_age` or
+    pub birthday_years: u32,                // calculated with div_spark.appointed_tick
+    pub birthday_month: u32,                // considered?
     pub birthday_day: u32,
     pub bio_age: u32,               
     pub chrono_age: Option<u32>,    
@@ -86,13 +104,13 @@ pub struct MortalAge {
 #[derive(Serialize, Deserialize)]
 pub struct Mortal {
     pub id: Uuid,
-    pub name: String,       // TO-DO: create a MortalName struct that references
-    pub title: String,      // naming conventions of a Civilization, as well as a
+//  pub name: MortalName,   // TO-DO: create a MortalName struct that references
+//  pub title: MortalTitle, // naming conventions of a Civ, as well as a
     pub species_id: Uuid,   // MortalTitle struct that has a bit of logic to it.
-    pub sex: String,        // MortalSex also may wind up being a struct, which would
-    pub civ_id: Uuid,       // draw from the parent species' defined qualities. (Not
-    pub age: MortalAge,     // all are male/female or even reproduce sexually, so it
-    pub home_loc_id: Uuid,  // will necessarily get complicated.)
+//  pub sex: MortalSex,     // MortalSex will draw from the parent species' defined
+    pub civ_id: Uuid,       // qualities. (Not all are male/female or even reproduce
+    pub age: MortalAge,     // sexually, so it will necessarily get complicated.)
+    pub home_loc_id: Uuid,
     pub current_loc_id: Uuid,
     pub orig_pop_id: Uuid,
     pub milieu_pop_id: Uuid,
@@ -100,7 +118,9 @@ pub struct Mortal {
     pub band_aff: Option<BandAffiliation>,
     pub div_spark: DivineSpark,
     pub identity: MortalIdentity,
-    pub capability: MortalCapability,
+//  pub occupation: Occupation, // This will probably come from an Enum and default to orig_pop's occupation.
+//  pub status: String,                 // Merge in a struct with title or something else?
+    pub skills: HashMap<Skill, f32>,
 //  pub agent: MortalAgent,             // This will be defined in agent defs.
 //  pub visibility: EntityVisibility,   // This will be defined for entities generally.
     pub pinned: bool,
